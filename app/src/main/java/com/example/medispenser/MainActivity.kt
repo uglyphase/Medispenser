2cc57c525fda7e08b2f4e5fef4527dd53bed3837
uglyphase <pewdpno.1@gmail.com>
2024-08-20 00:21:35 +0800

    first commit

---
+++ app/src/main/java/com/example/medispenser/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.medispenser
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.medispenser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
